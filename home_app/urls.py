f7244fced7dce558a6c908f9f4e9ca789339a4ea
Thoth <thoth2357@gmail.com>
2023-05-05 15:25:35 +0100

    logout login and profile urls done

--- home_app/urls.py
+++ home_app/urls.py
@@ -2,8 +2,14 @@
 from django.urls import path, include
 from .views import (
     home_view,
+    login_view,
+    logout_view,
+    profile_view,
 )
 
 urlpatterns = [
     path("", home_view, name="homepage"),
+    path("login", login_view, name="loginpage"),
+    path("logout", logout_view, name="logoutpage"),
+    path("student-dashboard/", profile_view, name="student-dashboard"),
 ]
