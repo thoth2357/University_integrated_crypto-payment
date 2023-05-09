b125b80f06a3334c77b58ea01a11c8b6a84bba52
Thoth <thoth2357@gmail.com>
2023-05-09 16:58:38 +0100

    added views for coinbase charge, webhook ,cancel and success

--- home_app/views.py
+++ home_app/views.py
@@ -1,6 +1,16 @@
+import logging
 from django.shortcuts import render,redirect
-from django.contrib import auth
-from django.contrib import messages
+from django.contrib import auth,messages
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+
+from coinbase_commerce.client import Client
+from coinbase_commerce.error import SignatureVerificationError, WebhookInvalidPayload
+from coinbase_commerce.webhook import Webhook
+
+from django.conf import settings
+
 
 
 # Create your views here.
@@ -27,4 +37,53 @@ def login_view(request, *args, **kwargs):
     return render(request, 'login.html')
 
 def profile_view(request, *args, **kwargs):
-    return render(request, "student-dashboard.html")
+    client = Client(api_key=settings.COINBASE_COMMERCE_API_KEY)
+    domain_url = 'http://localhost:8000/'
+    product = {
+        'name': 'School Fees',
+        'description': 'School Fees For Sheffield',
+        'local_price': {
+            'amount': '5.00',
+            'currency': 'USD'
+        },
+        'pricing_type': 'fixed_price',
+        'redirect_url': domain_url + 'success/',
+        'cancel_url': domain_url + 'cancel/',
+    }
+    charge = client.charge.create(**product)
+
+    return render(request, 'student-dashboard.html', {
+        'charge': charge,
+    })
+    
+
+def payment_cancel(request):
+    return render(request, 'cancel.html')
+
+def payment_success(request):
+    return redirect('student-dashboard')
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def coinbase_webhook(request):
+    logger = logging.getLogger(__name__)
+
+    request_data = request.body.decode('utf-8')
+    request_sig = request.headers.get('X-CC-Webhook-Signature', None)
+    webhook_secret = settings.COINBASE_COMMERCE_WEBHOOK_SHARED_SECRET
+
+    try:
+        event = Webhook.construct_event(request_data, request_sig, webhook_secret)
+
+        # List of all Coinbase webhook events:
+        # https://commerce.coinbase.com/docs/api/#webhooks
+
+        if event['type'] == 'charge:confirmed':
+            logger.info('Payment confirmed.')
+            # TODO: run some custom code here
+
+    except (SignatureVerificationError, WebhookInvalidPayload) as e:
+        return HttpResponse(e, status=400)
+
+    logger.info(f'Received event: id={event.id}, type={event.type}')
+    return HttpResponse('ok', status=200)
