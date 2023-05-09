5f02fca755ebb31abc3be669d5d1e8e8af03e49f
Thoth <thoth2357@gmail.com>
2023-05-09 16:57:28 +0100

    added cancel and success url path for coinbase

--- home_app/urls.py
+++ home_app/urls.py
@@ -5,6 +5,8 @@ from .views import (
     login_view,
     logout_view,
     profile_view,
+    payment_cancel,
+    payment_success
 )
 
 urlpatterns = [
@@ -12,4 +14,6 @@ urlpatterns = [
     path("login", login_view, name="loginpage"),
     path("logout", logout_view, name="logoutpage"),
     path("student-dashboard/", profile_view, name="student-dashboard"),
+    path("cancel/", payment_cancel, name="cancel"),
+    path("success/", payment_success, name="success"),
 ]
